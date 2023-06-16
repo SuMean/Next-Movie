d20ccdf974e821a51a722d5cc331432ed82975b4
sam101321 <sam010321@gmail.com>
2023-06-16 17:13:03 +0900

    align

--- app/movies/[...params]/page.tsx
+++ app/movies/[...params]/page.tsx
@@ -6,7 +6,7 @@ export default function Detail() {
   const title = searchParams.get("title");
   const image = searchParams.get("image");
   return (
-    <div>
+    <div className="container">
       <img
         src={`https://image.tmdb.org/t/p/w500${image}`}
         className="img"
@@ -14,12 +14,15 @@ export default function Detail() {
       <h4>{title || "Loading..."}</h4>
       <style jsx>
         {`
+          .container {
+            padding:20px;
+            text-align: center;
+          }
           .img {
             max-width: 100%;
             border-radius: 12px;
             transition: transform 0.2s ease-in-out;
             box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-            
           }
         `}
       </style>
